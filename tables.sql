658dd516a15b92e62498022fa25b61a8f14c9d60
Bobby Jr Corpus <bobby@corpus.ph>
2018-10-11 16:51:02 +0800

    added missing create sequence

--- tables.sql
+++ tables.sql
@@ -1,10 +1,16 @@
 create table customer (id int primary key, firstname varchar(10), lastname varchar(10), phoneno varchar(10), isdeleted number(1,0));
+create sequence customer_seq start with 1 
+increment by 1
+minvalue 1
+maxvalue 10000;
+
 create or replace trigger customer_trig before insert on Customer for each row
 begin
 select customer_seq.nextval into :new.id
 from dual;
 end;
 /
+
 insert into customer (firstname, lastname,phoneno, isdeleted) values ('Mickey', 'Mouse', '123456', 0);
 insert into customer (firstname, lastname,phoneno, isdeleted) values ('Donald', 'Duck', '123451', 0);
 
